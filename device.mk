50a9b1a5b77851ba78fae9d5da63fffb4b338b5a
zqm64 <zqm64@outlook.com>
2021-04-14 19:21:09 +0800

    tiare: overlay: Import power_profile

--- device.mk
+++ device.mk
@@ -28,6 +28,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/qpnp_pon.kl
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Properties
 include $(LOCAL_PATH)/properties.mk
 
